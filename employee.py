e740a3d4719f44f75b3bda1995bb8ce6494fbc9a
manini03 <96049792+manini03@users.noreply.github.com>
2021-12-13 15:03:56 +0530

    Add files via upload

---
+++ employee.py
@@ -0,0 +1,7 @@
+class employee:
+    def ___init___(self):
+        self.name='manini'
+def talk():
+    print("hi i am",self.name)
+e1=employee()
+e1.talk()
---
+++ student.py
@@ -0,0 +1,11 @@
+class student:
+    def __init__(self):
+        self.name='vishnu'
+        self.age= 20
+        self.marks=500
+def talk(self):
+    print('hi, my name is', self.name)
+    print('my age is:', self.age)
+    print('my marks are',self.marks)
+s1=student()
+s1.talk()
